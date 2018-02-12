f9aac7340a61221299b1473f21375336d179ff86
Andres Ibarra <aibarramartin@gmail.com>
2018-02-11 20:35:13 -0500

    Main Page Set Up
    set up the menu view, as well as created the skeleton for what will be the booking room view. Need to implement all the other options. Visuals are not permanent, simply placeholders.

---
+++ InnovationHub/BookARoom.swift
@@ -0,0 +1,75 @@
+//
+//  BookARoom.swift
+//  Innovation Hub
+//
+//  Created by Andres Ibarra on 2/11/18.
+//  Copyright © 2018 Andres Ibarra. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BookARoom: UIViewController, WKUIDelegate, WKNavigationDelegate{
+
+    @IBOutlet var returnButton: UIButton!
+    
+    @IBOutlet var bookingWebView: WKWebView!
+    
+    //this is for loading up the webview so the user doesn't think the application crashed!
+    var lblMessage: UILabel!
+    var vwProgressPopup: UIView!
+    var progressActivityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblMessage = UILabel(frame: CGRect(x: 50, y: 0, width: 200, height: 50))
+        vwProgressPopup = UIView(frame: CGRect(x: self.bookingWebView.frame.width/4.5, y: self.bookingWebView.frame.height/4, width: 220, height: 50))
+        progressActivityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
+        
+        bookingWebView.uiDelegate = self
+        bookingWebView.navigationDelegate = self
+        
+        let myURL = URL(string: "https://www.lib.fsu.edu/")
+        let myRequest = URLRequest(url: myURL!)
+        bookingWebView.load(myRequest)
+       
+        //return button rounded
+        returnButton.layer.borderWidth = 2
+        returnButton.layer.borderColor = Assist.convertFromHex(hexNum: 0xFFFFFF).cgColor
+        returnButton.layer.cornerRadius = 15
+        showProgress(msg: "Loading Webpage", withIndicator: true, remove: false)
+        
+    }
+    
+    //Hides the progress indicator
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        showProgress(msg: "Loading Webpage", withIndicator: true, remove: true)
+    }
+
+    
+    //shows the progress indicator, and with the remove parameter, decides, whether it will display it or not
+    func showProgress(msg: String, withIndicator: Bool, remove: Bool)
+    {
+        if !remove{
+            lblMessage.text = msg
+            lblMessage.textColor = UIColor.white
+            vwProgressPopup.layer.cornerRadius = 15
+            vwProgressPopup.backgroundColor = UIColor(white: 0, alpha: 0.7)
+            if (withIndicator)
+            {
+                progressActivityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+                progressActivityIndicator.startAnimating()
+                vwProgressPopup.addSubview(progressActivityIndicator)
+            }
+            vwProgressPopup.addSubview(lblMessage)
+            bookingWebView.addSubview(vwProgressPopup)
+        }else{
+            vwProgressPopup.removeFromSuperview()
+        }
+ 
+    }
+ 
+    
+}
---
+++ InnovationHub/Main Views/MainMenu.swift
@@ -0,0 +1,84 @@
+//
+//  FirstViewController.swift
+//  InnovationHub
+//
+//  Created by Andres Ibarra on 1/21/18.
+//  Copyright © 2018 Andres Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+
+    @IBOutlet var mainMenuTableView: UITableView!
+    var menuOptions = ["Book a Room", "Ask for Supervisor","Reserve Equipment","Learn More"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainMenuTableView.delegate = self
+        mainMenuTableView.dataSource = self
+    }
+
+    @IBAction func returnToMainView(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
+extension MainMenu: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.bounds.height/CGFloat(menuOptions.count)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainMenuOptions") as! mainMenuOptionCell
+        
+        cell.menuOptionText!.text = menuOptions[indexPath.row]
+        
+        
+        if cell.menuOptionText!.text == "Book a Room"{
+            cell.backgroundImage!.image = #imageLiteral(resourceName: "feature_hacking")
+        }else if cell.menuOptionText!.text == "Ask for Supervisor"{
+            cell.backgroundImage!.image = #imageLiteral(resourceName: "splash8")
+        }else if cell.menuOptionText!.text == "Reserve Equipment"{
+            cell.backgroundImage!.image = #imageLiteral(resourceName: "feature_makerspace")
+        }else if cell.menuOptionText!.text == "Learn More"{
+            cell.backgroundImage!.image = #imageLiteral(resourceName: "feature_colab_tech")
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if menuOptions[indexPath.row] == "Book a Room"{
+            //display book a room view
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "bookARoom")
+            self.present(vc!, animated: true, completion: nil)
+            
+        }else if menuOptions[indexPath.row] == "Ask for Supervisor"{
+            //display supervisor request view
+        }else if menuOptions[indexPath.row] == "Reserve Equipment"{
+            //display reserve equipment View
+        }else if menuOptions[indexPath.row] == "Learn More"{
+            //display learn more view
+            
+        }
+    }
+    
+   
+}
+
+class mainMenuOptionCell: UITableViewCell{
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var imageFilter: UIView!
+    @IBOutlet var menuOptionText: UILabel!
+}
